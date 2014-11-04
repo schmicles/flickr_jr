027e9833f0dea03e84b384c4a931b78c91f7cafd
Code Division <hello@11th.ventures>
2014-11-04 12:00:46 +0800

    upload works add description to album

--- db/migrate/20141104102345_create_albums.rb
+++ db/migrate/20141104102345_create_albums.rb
@@ -2,7 +2,8 @@ class CreateAlbums < ActiveRecord::Migration
   def change
     create_table :albums do |t|
       t.string   :title
-      t.integer   :user_id
+      t.string   :description
+      t.integer  :user_id
 
       t.timestamps
     end

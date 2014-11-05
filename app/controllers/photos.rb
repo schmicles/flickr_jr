93d862fce29f9ca4ac95a6891677ccc603a181e1
Code Division <hello@11th.ventures>
2014-11-05 15:47:28 +0800

    after uploading photo, redirects to photo page

--- app/controllers/photos.rb
+++ app/controllers/photos.rb
@@ -1,7 +1,7 @@
 # ----- See Photo -----
 get '/albums/:album_id/:photo_id' do
-  @photo = Photo.find(photo_id: params[:photo_id])
-  @album = Album.find(album_id: params[:album_id])
+  @photo = Photo.find_by(id: params[:photo_id])
+  @album = Album.where(album_id: params[:album_id])
   erb :'photos/show'
 end
 
@@ -14,12 +14,12 @@ end
 
 # ----- Receive and save the uploaded file -----
 post '/photos' do
-  photo = Photo.new(file_name: params[:post][:file_name][:filename], caption: params[:post][:caption], album_id: params[:post][:album_id])
-  photo.save
+  @photo = Photo.new(file_name: params[:post][:file_name][:filename], caption: params[:post][:caption], album_id: params[:post][:album_id])
+  @photo.save
   File.open('public/images/' + params[:post][:file_name][:filename], "w") do |f|
     f.write(params[:post][:file_name][:tempfile].read)
   end
-  redirect "/albums/#{params[:post][:album_id]}/#{photo.id}"
+  redirect "/albums/#{@photo.album_id}/#{@photo.id}"
 end
 
 # ----- Delete Photo -----
